a097cb33f09652c9ebf18ec58c434bba96ec0d17
LG-Crusty <pandittheroyal@gmail.com>
2025-09-16 00:10:16 +0530

    added dotenv and fixed the code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,17 +1,16 @@
 // import { createServer } from "node:http";
-import {createServer} from "http"
+import { createServer } from "http";
 import { app } from "./app.ts";
 import { startSockerServer } from "./socket.server.ts";
-import "../src/dns/dns.ts";
-import "../src/services/udpSocket.ts";
-
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = createServer(app);
 
 // app listening
 
-const serverStarted = async() => {  
+const serverStarted = async () => {
+  console.log(process.env.PORT);
   server.listen(process.env.PORT, () => {
     // here the server getting listend
     console.log(`App is listening on the port : ${process.env.PORT}`);
@@ -26,52 +25,3 @@ const serverStarted = async() => {
 serverStarted();
 
 export { server };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { createServer } from "node:http";
-// import { app } from "./app";
-// import {Server} from "socket.io";
-// import { Devices } from "./dns/dns.ts";
-
-// const server = createServer(app);
-// const port = process.env.PORT
-
-// server.listen(port, () => {
-//   console.log(`Server is listening on port: ${port}`);
-// });
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: process.env.CORS_ORIGIN, // or the frontend url
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-// io.on("connection", (socket) => {
-//   console.log(`Socket connected: ${socket.id}`);
-//   socket.emit("devices", Devices);
-
-//   socket.on("disconnected", () => {
-//     console.log(`Socket disconnected: ${socket.id}`);
-//   });
-// });
-
-// console.log("Socket server is ready!");
